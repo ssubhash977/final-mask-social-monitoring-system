6d2db2bd4136ab7455574781e97a992a1fdd8250
Subhash S <62792059+ssubhash977@users.noreply.github.com>
2021-05-19 17:22:34 +0530

    Update about.py

--- about.py
+++ about.py
@@ -3,5 +3,5 @@ def about_us():
 	st.title('About Us')
 	col1, col2 ,col3 = st.beta_columns(3)
 	col1.image('subhash.png',caption="Subhash S")
-	col1.header("Subhash S")
-	#col2.image('sneha.png',caption="Sneha Korgal")
+	col2.image('sneha.png',caption="Sneha Korgal")
+	col3.image('ullas.png',caption="Ullas A")
